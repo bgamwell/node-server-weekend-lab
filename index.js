e48b3ada2806ddba00d5efa36f1000fb72f4304c
bgamwell <bgamwell@gmail.com>
2015-07-14 08:50:13 -0700

    Updated JS

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 var express = require('express'),
-    app = express();
-    bodyParser = require('body-parser');
-    _ = require("underscore");
+    app = express(),
+    bodyParser = require('body-parser'),
+    _ = require("underscore"),
+    mongoose = require('mongoose'),
+    Post = require('./models/post');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-// This is our simulated database
-var posts = [
-  {
-    id: 1,
-    title: 'Hello, World!',
-    body: 'This is a really cool sample blog post!'
-  },
-  {
-    id: 2,
-    title: 'A Simple Guide to Node.js',
-    body: 'You can learn more on my website!'
-  },
-  {
-    id: 3,
-    title: 'How to Set Up a Simple Website Project',
-    body: 'Learn more by going to my GitHub repo!'
-  }
-];
+mongoose.connect('mongodb://localhost/blogposts'); // specifies that we're working with the post server
 
 // Setting up all routes
 
@@ -31,63 +16,69 @@ app.get('/', function (req, res) { // root, serves index.html as the homepage
   res.sendFile(__dirname + '/public/views/index.html');
 });
 
-app.get('/api/posts', function (req, res) { //get request for the posts API
-  res.json(posts); //body-parser allows us to format the server response as JSON
+
+app.get('/api/posts', function (req, res) {
+  Post.find(function (err, posts) {
+    res.json(posts);
+    // res.send("The server is working!");
+  });
 });
+
 
 app.get('/api/posts/:id', function (req, res) { //get request for a specific post
-  var targetPost = parseInt(req.params.id); //parseInt turns the ID string from the URL into an integer id
-  var foundPost = _.findWhere(posts, { id: targetPost });
-  res.json(foundPost);
+  var targetPost = req.params.id;
+
+  //finding one post within the schema and responding with a json object of the targetPost
+  Post.findOne( { _id: targetPost }, function(err, targetPost) {
+    res.json(targetPost);
+  });
 });
+
 
 app.post('/api/posts', function (req, res) { //post a new blog post
+
   // grab params (word and definition) from form data
-  var newPost = req.body;
+  var newPost = new Post({
+    title: req.body.title, // data that is returned in a request -- all data has a header and a body -- google http request structure
+    body: req.body.body
+  });
 
-  // set sequential id (last id in `phrases` array + 1)
-  if (posts.length > 0) {
-    newPost.id = posts[posts.length - 1].id +  1;
-  } else {
-    newPost.id = 0;
-  }
+  console.log(req.body);
 
-  // add newPhrase to `phrases` array
-  posts.push(newPost);
-
-  // send newPhrase as JSON response
-  res.json(newPost);
-
+  // Save newPost to the mongodb
+  newPost.save(function (err, savedPhrase) {
+    res.json(savedPhrase);
+  });
 });
+
 
 app.put('/api/posts/:id', function (req, res) { //update an existing blog post
   // set the value of the id
- var targetPost = parseInt(req.params.id);
+ var targetPost = req.params.id; // I'm not sure how to grab the ID here
 
  // find item in `phrases` array matching the id
- var foundPost = _.findWhere(posts, { id: targetPost });
+ Post.findOne( { _id: targetPost }, function(err, targetPost) {
+   //update the post title and body
+   targetPost.title = req.body.title;
+   targetPost.body = req.body.body;
 
- // update the phrase's word
- foundPost.title = req.body.title;
+  // save the new post
+   targetPost.save(function (err, savedPost) {
+      res.json(savedPost);
+    });
+  });
+ });
 
- // update the phrase's definition
- foundPost.body = req.body.body;
-
- // send back edited object
- res.json(foundPost);
-
-});
 
 app.delete('/api/posts/:id', function (req, res) { //delete a blog post by ID
-  var targetPost = parseInt(req.params.id);
-  var foundPost = _.findWhere(posts, { id: targetPost });
+  var targetPost = req.params.id;
 
-  var index = posts.indexOf(foundPost);
-
-  posts.splice(index, 1);
-
-  res.json(foundPost);
+  // find item in the db and delete it
+  Post.findOneAndRemove({_id: targetPost}, function (err, deletedPost) {
+    res.json(deletedPost);
+  });
 });
+
 
 app.listen(3000, function() {
   console.log('server started on localhost:3000');
